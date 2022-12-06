3ca83ba2ce16f8923e5325f5af843503a2071c39
kinstella <mark@kinstelli.com>
2022-12-05 22:25:35 -0600

    day5, part2, but didn't enjoy it

--- src/days/day05.rs
+++ src/days/day05.rs
@@ -1,5 +1,5 @@
 use std::{fs, str};
-const FILENAME: &str = "./resources/day05/example.txt";
+const FILENAME: &str = "./resources/day05/input.txt";
 
 fn read_data(fpath: &str) -> String {
     fs::read_to_string(fpath).expect("Could not read input file")
@@ -37,7 +37,7 @@ pub fn build_stacks(initial_stack: &str) -> Vec<Vec<String>> {
     stacks.to_owned()
 }
 
-pub fn process_ops(mut stacks: Vec<Vec<String>>, op: &str) -> Vec<Vec<String>> {
+pub fn process_ops(mut stacks: Vec<Vec<String>>, op: &str, model: u32) -> Vec<Vec<String>> {
     let op_nums: Vec<u32> = op
         .replace("move", "")
         .replace("from", "")
@@ -49,16 +49,26 @@ pub fn process_ops(mut stacks: Vec<Vec<String>>, op: &str) -> Vec<Vec<String>> {
 
     if op_nums.len() > 1 {
         let (opct, opfrom, opto) = (op_nums[0], op_nums[1] - 1, op_nums[2] - 1);
-        for i in 0..opct  {
-            print_stacks(&stacks);
-            let p = stacks[opfrom as usize].pop().unwrap();
-            stacks[opto as usize].push(p);
+
+        if model == 9000 {
+            for i in 0..opct {
+                let p = stacks[opfrom as usize].pop().unwrap();
+                stacks[opto as usize].push(p);
+            }
+        } else {
+            let stacklen = stacks[opfrom as usize].len();
+            let mut remstack = stacks[opfrom as usize][0..stacklen - opct as usize].to_vec();
+            let mvstack = stacks[opfrom as usize][stacklen - opct as usize..].to_vec();
+            for c in mvstack.iter() {
+                stacks[opto as usize].push(c.to_string());
+                stacks[opfrom as usize] = remstack.to_vec();
+            }
         }
     }
     stacks
 }
 
-pub fn print_stacks( stacks: &Vec<Vec<String>>) {
+pub fn print_stacks(stacks: &Vec<Vec<String>>) {
     for i in stacks {
         println!("{:?}", i);
     }
@@ -71,8 +81,24 @@ pub fn part1() {
     let ops_list = input_sections[1].split("\n").collect::<Vec<&str>>();
     let mut stacks = build_stacks(stack_def);
     for op in ops_list {
-        stacks = process_ops(stacks, op);
+        stacks = process_ops(stacks, op, 9000);
     }
+    println!("Part one answer:");
+    for s in stacks {
+        print!("{:?}", s.last().unwrap());
+    }
+}
+
+pub fn part2() {
+    let raw_data = read_data(FILENAME);
+    let input_sections: Vec<&str> = raw_data.split("\n\n").collect::<Vec<&str>>();
+    let stack_def = input_sections[0];
+    let ops_list = input_sections[1].split("\n").collect::<Vec<&str>>();
+    let mut stacks = build_stacks(stack_def);
+    for op in ops_list {
+        stacks = process_ops(stacks, op, 9100);
+    }
+    println!("\nPart two answer:");
     for s in stacks {
         print!("{:?}", s.last().unwrap());
     }
--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,5 @@ mod days;
 
 fn main() {
     days::day05::part1();
+    days::day05::part2();
 }
