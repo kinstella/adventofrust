90950e3f46827093d85f4fd03f1b1bc59786e658
kinstella <mark@kinstelli.com>
2022-12-06 11:15:57 -0600

    rm unnecessary mem functions

--- src/days/day06.rs
+++ src/days/day06.rs
@@ -7,12 +7,12 @@ fn read_data(fpath: &str) -> String {
 }
 
 pub fn find_unique_set(ln : usize, orig_str : String) {
-    let mut vstr = orig_str.chars().map(|c| c.to_string()).collect::<Vec<String>>().to_owned();
+    let mut vstr = orig_str.chars().map(|c| c.to_string()).collect::<Vec<String>>();
     let mut charct = 0;
-    let mut items = vstr.iter().take(ln).map(|c| c.to_string()).collect::<HashSet<String>>().clone();
+    let mut items = vstr.iter().take(ln).map(|c| c.to_string()).collect::<HashSet<String>>();
     while items.len() < ln {
         vstr = vstr[1..].iter().map(|c| c.to_string()).collect();
-        items = vstr.iter().take(ln).map(|c| c.to_string()).collect::<HashSet<String>>().clone();
+        items = vstr.iter().take(ln).map(|c| c.to_string()).collect::<HashSet<String>>();
         charct = 1 + charct;
     }
     println!("At char {}, we found {:?}", charct + ln, items);
